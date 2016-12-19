3752caf35ddd1517cc23d6dfa265d8ededa066a1
Amr Adel <amradelkhalil@gmail.com>
2016-12-19 20:20:15 +0200

    repair problem

--- src/java/Models/MovieModel.java
+++ src/java/Models/MovieModel.java
@@ -6,7 +6,13 @@
 package Models;
 
 import DBConnection.DBC;
+import com.mysql.jdbc.PreparedStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,4 +20,53 @@ import java.sql.Connection;
  */
 public class MovieModel {
     
+    public MovieModel(){
+        
+    }
+    
+    public HashMap<String,String> showMovie(int id){
+        
+        HashMap<String,String> movie = new HashMap<>();
+        String name="", description="", img_url="", duration="", renting_price_per_day="", category="";
+        double rate_sum = 0.0, rate = 0.0, rate_count = 0.0;
+        
+        
+        Connection con = DBC.getActiveConnection();
+        String query="Select * from Movie where id=?";
+        try {
+            PreparedStatement p = (PreparedStatement) con.prepareStatement(query);
+            p.setInt(id, 1);
+            
+            ResultSet row = p.executeQuery();
+            
+            while(row.next()){
+                name = row.getString("name");
+                description = row.getString("description");
+                img_url = row.getString("img_url");
+                duration = row.getString("duration");
+                renting_price_per_day = row.getString("renting_price_per_day");
+                category = row.getString("category");
+                rate_sum = row.getDouble("rate_sum");
+                rate = row.getDouble("rate");
+                rate_count = row.getDouble("rate_count");
+            }
+            
+            movie.put("name", name);
+            movie.put("description", description);
+            movie.put("img_url", img_url);
+            movie.put("duration", duration);
+            movie.put("renting_price_per_day", renting_price_per_day);
+            movie.put("category", category);
+            movie.put("rate_sum", String.valueOf(rate_sum));
+            movie.put("rate", String.valueOf(rate));
+            movie.put("rate_count", String.valueOf(rate_count));
+            return movie;
+        } catch (SQLException ex) {
+            Logger.getLogger(MovieModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        DBC.closeConnection();
+        
+        return null;
+    }
 }
