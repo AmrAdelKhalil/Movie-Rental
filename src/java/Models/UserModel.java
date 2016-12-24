e87e89447d56a3f6ed267345d559cb7892ac80c3
Abdo-Sameh <abdo.sameh2000@gmail.com>
2016-12-24 11:44:55 +0200

    Login_And_SignUp (#7)
    * Login_And_SignUp
    
    * remove comments
    
    * after clean

--- src/java/Models/UserModel.java
+++ src/java/Models/UserModel.java
@@ -17,6 +17,59 @@ public class UserModel {
         
     }
     
+    public HashMap<String, String> login(String email, String password){
+        
+        HashMap<String, String> user = new HashMap<>();
+        
+        Connection con = DBC.getActiveConnection();
+        String query = "select * from User where `email` = ? and `password` = ?;";
+        try {
+            PreparedStatement p = con.prepareStatement(query);
+            p.setString(1, email);
+            p.setString(2, password);
+            
+            ResultSet row = p.executeQuery();
+            if (row.next()){
+                user.put("name", row.getString("name"));
+                user.put("email", row.getString("email"));
+                user.put("password", row.getString("password"));
+                user.put("credit", row.getString("creditCard"));
+                return user;
+            }
+            row.close();
+            p.close();
+            DBC.closeConnection();
+            return user;
+        } catch (SQLException ex) {
+            Logger.getLogger(UserModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        DBC.closeConnection();
+        return null;
+    }
+    
+    public boolean signUp(String name, String email, String password, String creditCard){
+        Connection con = DBC.getActiveConnection();
+        String query = "insert into User(name, email, password, creditCard) values(?, ?, ?, ?);";
+        try {
+
+            PreparedStatement p = con.prepareStatement(query);
+            p.setString(1, name);
+            p.setString(2, email);
+            p.setString(3, password);
+            p.setString(4, creditCard);
+            p.executeUpdate();
+            p.close();
+            DBC.closeConnection();
+            return true;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UserModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        DBC.closeConnection();
+        return false;
+        
+    }
+    
     public HashMap<String, String> showSettings(int id){
         
         HashMap<String,String> user = new HashMap<>();
