f6cb045ccbaae8e128b5e2cea9fd09402789b348
Abdo-Sameh <abdo.sameh2000@gmail.com>
2016-12-24 18:56:15 +0200

    adminSignUp

--- src/java/Controllers/SignUp.java
+++ src/java/Controllers/SignUp.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Models.AdminModel;
 import Models.UserModel;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -22,17 +23,32 @@ public class SignUp extends HttpServlet {
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+        String isAdmin = request.getParameter("isAdmin");
         
-        String credit = request.getParameter("credit");
         HttpSession session = request.getSession(true);
         session.setAttribute("name", name);
-        session.setAttribute("password", password);
         session.setAttribute("email", email);
-        session.setAttribute("credit", credit);
         
-        UserModel user = new UserModel();
-        user.signUp(name, email, password, credit);
-        user.login(email, password);
+        if(isAdmin.equals("on")){
+            AdminModel admin = new AdminModel();
+            admin.signUp(name, email, password);
+            admin.login(email, password);
+            session.setAttribute("isAdmin", true);
+        }
+        else{    
+            UserModel user = new UserModel();
+            String credit = request.getParameter("credit");
+            user.signUp(name, email, password, credit);
+            user.login(email, password);
+            session.setAttribute("isAdmin", false);
+            
+        }
+        
+        
+        
+        
+        
+        
         response.sendRedirect("Views/newjsp.jsp");
     }
 
